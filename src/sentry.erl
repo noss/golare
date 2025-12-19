3268b00e43c41eda4dd651e4b2b63b381723dfb1
Christian Sunesson <christian.sunesson@kivra.com>
2025-12-19 17:04:17 +0100

    add functions to add to request context
    internally doing map merge

--- src/sentry.erl
+++ src/sentry.erl
@@ -2,6 +2,9 @@
 
 -export([get_request/0]).
 -export([set_request/1]).
+-export([add_request/1]).
+-export([add_request_data/1]).
+-export([add_request_env/1]).
 
 -export([get_user/0]).
 -export([set_user/1]).
@@ -37,23 +40,55 @@
     ip => user_ip_address()
 }.
 
--export_type([request_context/0, user_context/0]).
+-export_type([request_context/0, request_data/0, request_env/0]).
+-export_type([user_context/0]).
 
 -spec get_request() -> request_context() | undefined.
 get_request() ->
     erlang:get(pdict_key(request)).
 
--spec set_request(request_context()) -> request_context() | undefined.
+-spec set_request(request_context()) -> ok.
 set_request(ReqContext) ->
-    erlang:put(pdict_key(request), ReqContext).
+    erlang:put(pdict_key(request), ReqContext),
+    ok.
+
+-spec add_request(request_context()) -> ok.
+add_request(ReqContext) ->
+    case get_request() of
+        undefined ->
+            set_request(ReqContext);
+        PrevReqContext ->
+            set_request(maps:merge(PrevReqContext, ReqContext))
+    end.
+
+-spec add_request_data(request_data()) -> ok.
+add_request_data(ReqData) ->
+    case get_request() of
+        undefined ->
+            set_request(#{data => ReqData});
+        PrevReqContext ->
+            PrevData = maps:get(data, PrevReqContext, #{}),
+            set_request(PrevReqContext#{data := maps:merge(PrevData, ReqData)})
+    end.
+
+-spec add_request_env(request_env()) -> ok.
+add_request_env(ReqEnv) ->
+    case get_request() of
+        undefined ->
+            set_request(#{env => ReqEnv});
+        PrevReqContext ->
+            PrevEnv = maps:get(env, PrevReqContext, #{}),
+            set_request(PrevReqContext#{env := maps:merge(PrevEnv, ReqEnv)})
+    end.
 
 -spec get_user() -> user_context() | undefined.
 get_user() ->
     erlang:get(pdict_key(user)).
 
--spec set_user(user_context()) -> user_context() | undefined.
+-spec set_user(user_context()) -> ok.
 set_user(UserContext) ->
-    erlang:put(pdict_key(user), UserContext).
+    erlang:put(pdict_key(user), UserContext),
+    ok.
 
 pdict_key(user) -> sentry_user;
 pdict_key(request) -> sentry_request.
